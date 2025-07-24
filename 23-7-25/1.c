25d085674b43d768f6d6564000592faec9448676
Harshh-1104 <79073558+Harshh-1104@users.noreply.github.com>
2025-07-24 16:14:32 +0530

    Add files via upload

---
+++ 23-7-25/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    printf("\n my name : harsh");
+    printf("\n my age : 20");
+    printf("\n my bday : 11/04/2003");
+    printf("\n my address : everest park-b,behind mcd,kalawad road");
+
+    getch();
+
+}
