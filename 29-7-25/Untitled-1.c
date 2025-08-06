30f7ce1ad915c6572cf633458adad4e190b18679
Harshh-1104 <79073558+Harshh-1104@users.noreply.github.com>
2025-08-06 20:17:20 +0530

    Add files via upload

---
+++ 29-7-25/Untitled-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int i,j;
+
+    
+
+
+    for(i=1;i=<5;i++)
+
+    {
+        for(j=1;i<=j;j++)
+        {
+            printf("%d",i);
+        
+        }
+        printf("\n");
+
+    }
+
+    getch();
+}
