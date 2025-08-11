d8c298b1ce63b61bc75682c298bb8c595473bdbf
Harshh-1104 <79073558+Harshh-1104@users.noreply.github.com>
2025-08-11 15:33:38 +0530

    Add files via upload

---
+++ 8-8-25/student report.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+
+{
+    
+    int num,sub[100][3],total[100],pass=0,fail=0,topper=0,loser=0,high=0,low=0;
+    char name[100][30];
+    float avg[100];
+
+
+    printf("enter number of students : ");
+    scanf("%d",&num);                               // use of '&' when u have to put value in address 
+
+
+    for(int i=0; i< num;i++)
+    {
+        printf("\nEnter name of student %d : ",i+1);
+        scanf("%s",name[i]);                       // here '&' is not needed bcz string 'NAME'(that we define that we give) is address it self
+        total[i]=0;
+
+       for(int j = 0; j<3 ;j++)
+       {
+        
+        printf("\nEnter marks of sub %d : ",j+1);
+        scanf("%d",&sub[i][j]);
+
+        
+        total[i] += sub[i][j];
+       }
+
+     avg[i]=0;
+     avg[i] = total[i]/3;
+
+     if(avg[i] >= 40)
+     pass++;
+     else
+     fail++;
+
+
+     if(avg[i] > high)
+     {
+
+     high=avg[i];
+     topper=i;                              //for printf in end index
+     }
+
+
+     if(avg[i] < low)
+     {
+     low=avg[i];
+     loser=i;
+     }
+
+
+    }
+
+    printf("\n--------Student report----------");
+ 
+    printf("\nName       Total       avg        result");
+
+    for(int i=0;i < num;i++)
+    {
+
+    printf("\n%s         %d          %f           %s",name[i],total[i],avg[i], (avg[i] >= 40) ? "pass" : "fail");
+        
+
+
+
+
+
+
+    }
+    printf("\nTopper: %s  (%f)", name[topper], avg[topper]);
+    printf("\nLoser:  %s  (%f)", name[loser], avg[loser]);
+    printf("\nPassed: %d", pass);
+    printf("\nFailed: %d", fail);
+
+
+return 0;
+
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
